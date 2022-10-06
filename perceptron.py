51bba776a23f6109a97d0ba5c1655f7ecd804348
ram kaushal <ramkaushalguralla@gmail.com>
2022-10-06 11:20:42 +0530

    added the new calss perceptron with init and fir methods

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,22 @@
 import os
+from unicodedata import east_asian_width
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+class perceptron:
+    def __init__(self,eta,epoch):
+        self.weights = np.random.rand(3) * 1e-4
+        self.eta = eta 
+        self.epoch = epoch 
+    def fit(self,X,y):
+        self.X = X
+        self.y = y 
+        X_with_bias = np.c_[self.X,np.ones(4,1)]
+        print("X with bias is: ".format(X_with_bias))
+        return X_with_bias
+
+
+if __name__ == "__main__":
+    
